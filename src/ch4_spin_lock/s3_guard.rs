3e97d419360edf8f498b716b7e6c3bf2d9a5168e
Kaloian Doganov <doganov@gmail.com>
2025-07-22 14:49:32 +0300

    Convert the SpinLock usage example to a test

--- src/ch4_spin_lock/s3_guard.rs
+++ src/ch4_spin_lock/s3_guard.rs
@@ -52,3 +52,20 @@ impl<T> Drop for Guard<'_, T> {
         self.lock.locked.store(false, Release);
     }
 }
+
+#[test]
+fn main() {
+    use std::thread;
+
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+}
